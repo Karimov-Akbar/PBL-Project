437c95d3f2ce1ed6bab66bbd43d40fb9fa2f2c4c
Akbar <50082248+Karimov-Akbar@users.noreply.github.com>
2024-12-09 19:40:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get('/:page', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
